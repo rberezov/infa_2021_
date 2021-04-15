96db6e8c6e837dbdb1e329a39d33e11f2ef6ca97
rberezov <78897274+rberezov@users.noreply.github.com>
2021-04-15 12:10:23 +0300

    Add files via upload

---
+++ TEST_1/N1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n = 0;
+    int res = 1;
+
+    std::cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        res = res * i;
+    }
+
+    std::cout << res;
+
+    return 0;
+}
---
+++ TEST_1/N2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    int n = 0;
+
+    double res = 0;
+    double x = 0;
+
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> x;
+
+        res = res + x;
+    }
+
+    std::cout << std::fixed << std::setprecision(10);
+
+    std::cout << res / n;
+
+    return 0;
+}
